c656f30674de733014482dc0a8ea472bd4b014d3
GokiSakuraba <goki14914@icloud.com>
2018-10-17 00:19:20 +0900

    some icons in contact.vue

---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="contact">
+        <ul>
+            <li> <a href="https://twitter.com/7dx_a" target="_blank"><i class="fab fa-twitter fa-5x fa-spin"></i> </a></li>
+            <li> <a href="https://www.facebook.com/profile.php?id=100012664659914" target="_blank"><i class="fab fa-facebook-f fa-5x"></i></a> </li>
+            <li> <a href="https://github.com/gonzaemon111" target="_blank"><i class="fab fa-github fa-5x fa-spin"></i> </a> </li>
+        </ul>
+    </div>
+</template>
+<style scoped>
+.contact {
+    padding: 10px;
+}
+
+.contact ul li {
+    display: inline;
+    width: 15%;
+}
+
+.contact ul li:nth-child(3n-1) {
+    margin: 0 15%;
+}
+</style>
