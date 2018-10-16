0260c9f80796597b13e4a6363a5ed1dc9cf46e20
GokiSakuraba <goki14914@icloud.com>
2018-10-17 00:17:20 +0900

    make template in elementui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <nuxt/>
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#B3C0D1" text-color="#fff" active-text-color="#ffd04b">
+          <el-menu-item index="1"><nuxt-link to="/">Home</nuxt-link></el-menu-item>
+          <el-submenu index="2">
+            <template slot="title"><nuxt-link to="works">Works</nuxt-link></template>
+            <el-menu-item index="2-1"><nuxt-link to="works">freee-chat</nuxt-link></el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3"><nuxt-link to="contact">Contact</nuxt-link></el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <nuxt/>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+    <!-- nuxtの部分にpages配下のものを読み込む -->
   </div>
 </template>
 
@@ -49,5 +65,54 @@ html {
   color: #fff;
   background-color: #35495e;
 }
+
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 36.8em;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
 
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
