0acc4dd550f0e2c085fd0285c7af76b3f2ee8b98
GokiSakuraba <goki14914@icloud.com>
2018-10-17 02:55:30 +0900

    fix facebook icon

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,7 @@
     <div class="contact">
         <ul>
             <li> <a href="https://twitter.com/7dx_a" target="_blank"><i class="fab fa-twitter fa-5x fa-spin"></i> </a></li>
-            <li> <a href="https://www.facebook.com/profile.php?id=100012664659914" target="_blank"><i class="fab fa-facebook-f fa-5x"></i></a> </li>
+            <li> <a href="https://www.facebook.com/profile.php?id=100012664659914" target="_blank"><i class="fab fa-facebook-square fa-5x"></i></a> </li>
             <li> <a href="https://github.com/gonzaemon111" target="_blank"><i class="fab fa-github fa-5x fa-spin"></i> </a> </li>
         </ul>
     </div>
